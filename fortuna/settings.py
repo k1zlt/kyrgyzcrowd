70d2604f6fc0e266689aca8287f6ea31e25b8750
firuz.azizbekov <firuz.azizbekov_2026@ucentralasia.org>
2023-09-20 11:05:47 +0500

    .env

--- fortuna/settings.py
+++ fortuna/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(dutk6d^-$$=)(ftqu47x9&m^cb8q)5shs4%t8-d%6j=$zrt4a"
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
